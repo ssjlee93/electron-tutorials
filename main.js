0590f4cc82d9605471aaf7dfdf1db505b4781eac
Steve S. Lee <sjlee12@me.com>
2025-01-12 16:10:27 -0500

    added preload.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // console.log('Hello from Electron 👋')
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
